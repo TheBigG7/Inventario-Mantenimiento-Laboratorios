00fbb911df9c25c7c9dee83caec154387d5b8b38
derekvergara <112039686+derekvergara@users.noreply.github.com>
2024-07-26 15:04:04 -0500

    cambios en el email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { EmailService } from './equipos/email.service'; 
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Inventario-Mantenimiento-Laboratorios';
+
+  constructor(private emailService: EmailService) {}
+  enviarCorreo() {
+    const equipo = {
+      id: 1,
+      name: 'Equipo 1',
+      description: 'Descripción del equipo 1',
+      priority: 'Alta'
+    };
+
+    this.emailService.sendAlertEmail(equipo).subscribe(
+      response => {
+        console.log('Correo enviado exitosamente:', response);
+      },
+      error => {
+        console.error('Error al enviar el correo:', error);
+      }
+    );
+  }
 }
